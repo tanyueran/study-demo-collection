3b1ad0a366e2a15ccf6cd49dec9d893c0579ae07
谭鑫 <txloveu@163.com>
2020-01-15 10:09:59 +0800

    add some comments for [study-typescript]

--- study-typescript/src/main.ts
+++ study-typescript/src/main.ts
@@ -1,51 +1,69 @@
 import './style/index.css';
 import file from './imgs/1.jpg';
 
-interface comp{
-  name:string,
+/*
+* 声明接口  可用于：1、类去继承、2、约束对象等
+* 2、let obj: comp = {
+*   name: "name"
+* };
+* */
+interface comp {
+  name: string,
 }
 
-// 组件
-class Component implements comp{
+/*
+* 1、类继承接口
+* 接口作用：约束类必须要有接口中的属性和方法
+* */
+class Component implements comp {
   name: string;
-  _obj:HTMLElement;
+  _obj: HTMLElement;
 
-  constructor(name:string) {
+  constructor(name: string) {
     this.name = name;
     this._obj = document.createElement(name);
   }
 
   // 添加子元素
-  appendChild(comp:HTMLElement){
+  /*
+  * @param: "com: HTMLElement" HTMLElement约束参数的类型
+  * @return： "appendChild(): void" void限制放回的类型表示没有返回值
+  * */
+  appendChild(comp: HTMLElement): void {
     this._obj.appendChild(comp);
   }
 
   // 删除子元素
-  delChild(comp:HTMLElement){
+  delChild(comp: HTMLElement): void {
     this._obj.removeChild(comp);
   }
 
-  public get instance():HTMLElement{
+  public get instance(): HTMLElement {
     return this._obj;
   }
 }
 
-let div:Component = new Component('div');
+let div: Component = new Component('div');
 
-// 给img添加动态属性
-let img:{
-  [key:string]:any
+/*
+* 如果对象的key：value均不确定时，可以使用下面的方式
+* */
+let img: {
+  [key: string]: any
 } = new Image();
 img.src = file;
 
+/*
+* 类型出现解析问题时（满足规范使用时，不满足ts）使用下面的方式
+* */
 div.appendChild(img as HTMLElement);
 
-let p:HTMLElement = document.createElement('p');
+let p: HTMLElement = document.createElement('p');
 p.innerText = `lorem `;
 
 div.appendChild(p);
 
-let img2:any = new Image();
+let img2: any = new Image();
 img.src = file;
 
 div.appendChild(img2 as HTMLElement);
