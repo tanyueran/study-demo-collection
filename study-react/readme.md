5b8f8ad1be2f946e8c1564247cb9f67bd181383a
谭鑫 <txloveu@163.com>
2020-01-15 10:44:28 +0800

    add some comments for [study-react]

--- study-react/readme.md
+++ study-react/readme.md
@@ -14,13 +14,24 @@ webpack
 react
 react-router
 redux
+react-redux
 
 详细请看package.json里面的依赖
 
 ```
 
+## 说明
+```text
+笔者会使用的前端框架就是vue技术栈（vue、vue-router、vuex）和 react技术栈（react、react-router、redux、react-redux）  
+简单的两者相比较(或许不准确，鄙人浅见而已)：  
+vue   优点：上手快、完善的中文官方文档、一体化的构建使用起来很舒服  
+      缺点：对于大型应用中，大量的使用watch，会让项目性能差；开发的规范化遵守不好，代码会很乱   
+react 优点：高性能的框架、优秀的社区，拥有ant-design的完全支持，很棒的jsx开发  
+      缺点：一些官方文档常常不可访问（要翻墙，还是English），要求开发人员水平较高  
+```
+
 ## 注意
-babel升级到7后的
+babel升级到7后的，注意其他的版本一致性
 ```text
       "@babel/core": "^7.7.7",
       "@babel/plugin-proposal-class-properties": "^7.7.4",
@@ -28,5 +39,3 @@ babel升级到7后的
       "@babel/preset-react": "^7.7.4",
       "babel-loader": "^8.0.6",
 ```
-
-> 未完待续
--- study-react/src/app.js
+++ study-react/src/app.js
@@ -1,5 +1,8 @@
 // 引入react-router
 import React, {Suspense} from 'react'
+/*
+* 使用BrowserRouter 开发时需要webpack做特殊的设置，生产的时候需要服务器配置一下。
+* */
 import {BrowserRouter as Router, HashRouter, Switch, Route, Link} from 'react-router-dom';
 
 import MLoading from './components/MLoading.js'
@@ -11,15 +14,17 @@ export default class App extends React.Component {
   render() {
     return <HashRouter>
       <div>
+        {/*Suspense，配合React.lazy使用，正在加载是，渲染fallback中的组件*/}
         <Suspense fallback={<MLoading/>}>
           <Switch>
             {/*定义第一层的路由，对应到pages/index.js出定义第二层路由*/}
             {routes.map((route, i) =>
+              // 定于路由
               <Route
                 key={i}
                 path={route.path}
                 render={props => (
-                  // pass the sub-routes down to keep nesting
+                  // 通过props.render渲染，并将子路由传递下去
                   <route.component {...props} routes={route.routes}/>
                 )}
               />)}
